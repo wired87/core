4eb5e77cacd68287d23cd8b00c73bb0c2897f673
wired87 <derbenedikt.sterra@gmail.com>
2025-05-31 18:53:40 +0200

    setup

--- bm/urls.py
+++ bm/urls.py
@@ -9,11 +9,3 @@ urlpatterns = [
     path('world/', include("qf_sim.dj.urls")),
 ]
 
-"""    path('client/', include("client.dashboard.urls.client")),
-path('work/', include("client.dashboard.urls.admin")),
-path('auth/', include("client.dashboard.auth.urls")),
-path('gvs/', include("utils.ggoogle.vertexai.dj.urls")),
-path('dai/', include("utils.ggoogle.documentai.views.urls")),
-path('sp/', include("utils.ggoogle.spanner.dj.urls")),
-path('queue/', include("utils.ggoogle.tasks.dj.urls")),
-"""
