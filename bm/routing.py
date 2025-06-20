283d64248b05123d2da790643fa37a59f2738628
wired87 <derbenedikt.sterra@gmail.com>
2025-06-20 11:36:05 +0200

    status message sys

--- bm/routing.py
+++ bm/routing.py
@@ -1,10 +1,8 @@
 # your_app/routing.py
-from qf_sim.cluster.dj.sim_routing import cluster_ws_uris
 from qf_sim.dj.sim_routing import sim_ws_urls
 
 websocket_urlpatterns = [
     *sim_ws_urls,
-    *cluster_ws_uris,
 ]
 
 
