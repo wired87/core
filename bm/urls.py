1cdedf27769b4feb74ec30fc34c85140fbbbed24
wired87 <derbenedikt.sterra@gmail.com>
2025-07-10 14:22:38 +0200

    prep cloud

--- bm/urls.py
+++ bm/urls.py
@@ -3,12 +3,12 @@ from django.contrib import admin
 from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('betse/', include("_betse.urls")),
+    #path('betse/', include("_betse.urls")),
 
     path('world/', include("qf_sim.dj.urls")),
-    path('graph/', include("utils.graph.dj.urls")),
+    #path('graph/', include("utils.graph.dj.urls")),
 
     # frontend
-    path('unicorn/', include("django_unicorn.urls")),
+    #path('unicorn/', include("django_unicorn.urls")),
 ]
 
