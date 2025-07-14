e45fe6ac77675fa3b2b5bdb47a055f0bb8a54858
wired87 <derbenedikt.sterra@gmail.com>
2025-07-14 22:23:05 +0200

    front

--- frontend/main.py
+++ frontend/main.py
@@ -3,12 +3,17 @@ from typing import List
 from django_unicorn.components import UnicornView
 import random
 
+from bm.settings import TEST_USER_ID
+from qf_sim.sim_runner import SimCore
+
+
 class ChatView(UnicornView):
     template_name = r"C:\Users\wired\OneDrive\Desktop\BestBrain\frontend\templates\unicorn\i.html"
 
     message: str = ""
     chat_log: List[str] = []
     dots: List[dict] = []
+    nodes:list = []
 
     def send(self):
         print("HI!")
@@ -17,6 +22,20 @@ class ChatView(UnicornView):
         self.chat_log.append(f"You: {self.message}")
         self.chat_log.append(f"Bot: Echo: {self.message}")
         self.message = ""
+
+
+    def run_sim(self):
+        test = SimCore(
+            user_id=TEST_USER_ID,
+            env_id=f"env_bare_{TEST_USER_ID}",
+            visualize=False,
+            demo=True
+        )
+        self.nodes = [attrs for nid, attrs in test.g.G.nodes(data=True)]
+        test.run_connet_test()
+
+
+
 
     def spawn_dot(self):
         self.dots.append({
