d3a55097d9872ff8b7d4d981981fc623591a7185
wired87 <derbenedikt.sterra@gmail.com>
2025-07-21 12:15:58 +0200

    move sim

--- gdb_manager/bq/schemas/info_table.py
+++
@@ -1,7 +0,0 @@
-from google.cloud import bigquery
-
-INFO_TABLE_SCHEMA = [
-    bigquery.SchemaField("dataset", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("info", "STRING", mode="NULLABLE"),
-    bigquery.SchemaField("graph_conv", "BOOL", mode="NULLABLE", default_value_expression="FALSE")
-]
