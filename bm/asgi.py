32c6d4f24ecb4d1a47e94644885b2c1b28e392cd
wired87 <derbenedikt.sterra@gmail.com>
2025-08-19 10:14:14 +0200

    bq request

--- bm/asgi.py
+++ bm/asgi.py
@@ -10,7 +10,6 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import os
-
 from bm import routing
 
 if os.name == "nt":
