9a8c14e045fcd724099ab7d5967953cfb16cd049
wired87 <derbenedikt.sterra@gmail.com>
2025-09-03 21:33:50 +0200

    frontend relay creattion process implementnation

--- testing/pod_deployment.py
+++ testing/pod_deployment.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import threading
+import time
 
 from app_utils import USER_ID, ENV_ID
 from fb_core.real_time_database import FirebaseRTDBManager
@@ -10,16 +11,17 @@ from qf_sim.world.create_env import EnvCreatorProcess
 from utils.deserialize import deserialize
 from utils.dj_websocket.handler import ConnectionManager
 from utils.graph.local_graph_utils import GUtils
-from utils.utils import Utils
 
 from dotenv import load_dotenv
+
+from utils.utils import Utils
+
 load_dotenv()
 
 def create_world_process(world_cfg=None, user_id=USER_ID):
     print("DEBUG: Starting create_world_process()")
     env_creator = EnvCreatorProcess(
         user_id,
-        Utils(),
     )
     world_cfg = deserialize(world_cfg)
 
@@ -32,20 +34,9 @@ def create_world_process(world_cfg=None, user_id=USER_ID):
         world_cfg=[
             world_cfg
         ],
-        env_id=ENV_ID  # testing same alltimes
     )
-    print("DEBUG: world_cfg_process finished.")
-    print("Create World Process finished")
+    print("world_cfg_process finished.")
     return env_creator
-
-
-
-
-
-
-
-
-
 
 
 
@@ -56,21 +47,22 @@ class Connector:
     """
     After pod deployment :  Connect
     """
-    def __init__(self, env_cfg, user_id, env_id):
+
+    def __init__(self, env_cfg, user_id):
         self.ready_sessions = []
 
         self.env_cfg = env_cfg
         self.user_id = user_id
-        self.env_id = env_id
+
+        self.cluster_root = "cluster.botworld.cloud"
 
         self.instance = os.environ.get("FIREBASE_RTDB")
-        self.database = f"users/{self.user_id}/env/{env_id}"
 
+        self.utils = Utils()
         self.connection_manager = ConnectionManager()
         self.gke_admin = GKEAdmin()
         self.db_manager = FirebaseRTDBManager(
             database_url=self.instance,
-            base_path=self.database,
         )
 
     async def connect_to_pods(self):
@@ -88,37 +80,25 @@ class Connector:
                 for struct in self.env_cfg.values()
             )
 
-
-
-        # dict : pod_name : ip
-        self.all_ips: dict = self.gke_admin.get_service_public_ips(
-            service_names=all_pods,
-        )
-
         # check globs ready
         await self.check_ready(self.env_cfg)
 
-        # Spam requests
-        await self.connection_manager.request_urls_process(self.all_ips)
-
-        self.connection_manager.start_connection_thread(
-            self.all_ips
+        self.start_connection_thread(
+            pod_names=all_pods
         )
         print("All connections threads started")
 
-    async def check_ready(self, env_cfg:list[str]):
+    async def check_ready(self, env_ids:list[str]):
         print("Start ready Thread")
 
         def _connect():
             """
             Wait till all clusters are build up
             """
-            env_cfg_copy = env_cfg.copy()
+            ready_envs:list = []
             try:
-                while self.connection_manager.all_ready is not True:
-                    ready = False
-                    pod_name = ""
-                    for env_id, struct in env_cfg.items():
+                while len(ready_envs) < len(env_ids):
+                    for env_id in env_ids:
                         print("_connect", env_id)
                         data = self.db_manager.get_data(
                             path=f"global_states/",
@@ -129,17 +109,14 @@ class Connector:
                             ready: bool = data["global_states"]["ready"]
                             if ready is True:
                                 self.ready_sessions.append(env_id)
-
-                                if len(self.ready_sessions) > len(env_cfg_copy.keys()):
-                                    self.connection_manager.all_ready = True
-
-                    if ready is True:
-                        env_cfg_copy.pop(env_id)
-                        ready = False
-                        pod_name = ""
+                                ready_envs.append(env_id)
+                        time.sleep(2)
+                        print(f"{len(ready_envs)}/{len(env_ids)}")
+                print("Finished Ready check")
+                return True
             except Exception as e:
                 print(f"Error chck for global state: {e}")
-
+            return False
         #
         self.ready_thread = threading.Thread(
             target=_connect,
@@ -150,6 +127,113 @@ class Connector:
         # Start Thread
         self.ready_thread.start()
         print("Threadstarted succesfuully")
+
+
+    async def connect_all_pods_process(
+            self,
+            pod_names: list[str]
+    ) -> list:
+        print("Connection request process started")
+        all_authenticated = []
+        index = 0
+        try:
+            while len(all_authenticated) < len(pod_names):
+                if index < 30:
+                    for pod_name in pod_names:
+                        success:bool = await self.connect_to_pod(
+                            pod_name
+                        )
+                        if success is True:
+                            all_authenticated.append(
+                                all_authenticated
+                            )
+                        # Small delay between iters
+                        time.sleep(1)
+                        index += 1
+                        print(f"{len(all_authenticated)}/{len(pod_names)} pods connected")
+                else:
+                    print("Max request attampts reached. Break process")
+                    # Create List of missing pods that couldnt be connected to
+                    missing_pods = [name for name in pod_names if name not in all_authenticated]
+                    return missing_pods
+
+            # return empty list if while loop finished
+            return []
+
+        except Exception as e:
+            print(f"Error: {e}")
+        print("Finished Connection request process")
+
+
+
+
+    def start_connection_thread(self, pod_names):
+        # FB Upsert thread
+        print("Create Con thread")
+
+        def _connect():
+            missing_pods:list = asyncio.run(
+                self.connect_all_pods_process(pod_names)
+            )
+            if len(missing_pods):
+                # todo error intervention
+                pass
+            else:
+                pass
+
+        self.con_thread = threading.Thread(
+            target=_connect,
+            name="POD_INIT_CONNECTION",
+            daemon=True  # Optional: Der Thread wird beendet, wenn das Hauptprogramm endet
+        )
+
+        # Start Thread
+        self.con_thread.start()
+        print("Connect to Pods thread started")
+
+
+    async def connect_to_pod(self, pod_name):
+        """
+        Connect to a GKE cluster based on its ip:port
+        :param ip:
+        :param pod_name:
+        :return:
+        """
+
+        auth_payload = {
+            "type": "auth",
+            "data": {
+                "key": pod_name
+            }
+        }
+
+        try:
+            endpoint = f"{self.cluster_root}/{pod_name}/"
+            cr = await self.utils.apost(
+                url=endpoint,
+                data=auth_payload,
+            )
+            if cr and "response_key" in cr and "key" in cr and "session_id" in cr:
+                if cr["key"] == pod_name:
+                    # Successful pod authenticated -> append valid
+                    print(f"Pod {pod_name} connected successfully")
+                    return True
+                else:
+                    print(f"Invlalid key received: {cr['key']}")
+            else:
+                raise ValueError("No con request triger controlled Exceptio")
+        except Exception as e:
+            print(f"Error fetching: {e}")
+        return False
+
+
+
+
+
+
+
+
+
 
 
 def create_cfg():
