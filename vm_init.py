cf4dc65dc36496e462d67aebda67440d442671b8
wired87 <derbenedikt.sterra@gmail.com>
2025-09-07 13:15:44 +0200

    debug kubernetes

--- vm_init.py
+++ vm_init.py
@@ -7,13 +7,10 @@ init=f"""
 source workenv/bin/activate && cd BestBrain && export PYTHONPATH=$PYTHONPATH:$(pwd) && git pull
 """
 
-
 gunicorn_restart=rf"""
-
 sudo nginx -t
 sudo systemctl reload nginx
 sudo systemctl restart gunicorn
 sudo systemctl restart fail2ban
 sudo journalctl -u gunicorn -f
-
 """
