350566bd67bd0ecf52cc81a1e571e505f4158a23
wired87 <derbenedikt.sterra@gmail.com>
2025-09-08 07:41:15 +0200

    Ingress ctlr debug

--- testing/server.py
+++ testing/server.py
@@ -1,9 +1,11 @@
 import requests
 
 if __name__ == "__main__":
-    url = "https://cluster.clusterexpress.com/env-rajtigesomnlhfyqzbvx-qyhimnidxniwbmlzhlqy/root/"
-    response = requests.get(url )
+    url = "https://cluster.clusterexpress.com/env-rajtigesomnlhfyqzbvx-qjodlmdexctwpfvaeqan/root/"
+    response = requests.get(url)
 
 # CLUSTER ERRORS
 # Exception while start state main: 'DB_WORKER'
 # Error in get_npm: string indices must be integers
+
+
