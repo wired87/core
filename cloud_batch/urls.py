45be5b0096075e2b57a7e712f7cbd24da6ea5750
bestb <derbenedikt.sterra@gmail.com>
2025-10-15 16:24:35 +0200

    hi

--- cloud_batch/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from cloud_batch.views import JobDetailsView, JobDeleteView, JobCreatorView
-app_name = "batch"
-urlpatterns = [
-    path('create/', JobCreatorView.as_view(), name='job-create'),
-    path('delete/', JobDeleteView.as_view(), name='job-create'),
-    path('details/', JobDetailsView.as_view(), name='job-details'),
-]
